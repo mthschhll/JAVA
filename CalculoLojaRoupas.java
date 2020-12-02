699aba1d45801abf866ae1b7e2e435911e5af674
Matheus Schiehll <math.schiehll@gmail.com>
2020-12-02 17:41:28 -0300

    Update CalculoLojaRoupas.java

--- CalculoLojaRoupas.java
+++ CalculoLojaRoupas.java
@@ -9,12 +9,25 @@ public class CalculoLojaRoupas {
 		int cod1, cod2, num1, num2;
 		double valor1, valor2, total;
 		
+		System.out.println("Diigite o codigo da primeira peca: ");
 		cod1 = sc.nextInt();
+		
+		System.out.println("Diigite a quantidade: ");
 		num1 = sc.nextInt();
+		
+		System.out.println("Diigite o valor: ");
 		valor1 = sc.nextDouble();
+		
+		System.out.println("Diigite o codigo da segunda peca: ");
 		cod2 = sc.nextInt();
+		
+		System.out.println("Diigite a quantidadea: ");
 		num2 = sc.nextInt();
+		
+		System.out.println("Diigite o valor: ");
 		valor2 = sc.nextDouble();
+		
+		System.out.println("\n---------------------------------\n");
 		
 		total = (num1 * valor1 + num2 * valor2);
 		
